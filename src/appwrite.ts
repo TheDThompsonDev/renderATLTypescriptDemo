bac7a5e7b55607afcd40cae737ef32ee653989f1
Danny Thompson <dannythompson901@gmail.com>
2024-10-24 17:52:44 -0500

    Update appwrite.ts

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -5,10 +5,6 @@ const client = new Client();
 // const endpoint = import.meta.env.REACT_APP_APPWRITE_ENDPOINT!;
 // const projectId = import.meta.env.REACT_APP_APPWRITE_PROJECT_ID!;
 
-client
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('658ef6fc40a8cc9b2602');
-
 const databases = new Databases(client);
 
 export { databases, ID, Query };
